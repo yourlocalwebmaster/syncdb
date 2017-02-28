2306bf6e29cccc7fd0339aa7fda3f4c566bba92d
Ubuntu <ubuntu@ip-172-31-44-84.us-west-2.compute.internal>
2017-02-28 21:40:20 +0000

    putput formatting

--- syncdb.sh
+++ syncdb.sh
@@ -2,6 +2,10 @@
 dir=$(dirname $0)
 . $dir/syncdb.cfg
 
+echo "\r\n";
+echo "\033[93mBeginning Sync Process...\033[0m";
+echo "\r\n";
+
 if [ -w ${saveBackupPath} ]; then
     ## Backup Current Database
     if mysqldump -h ${db_A_host} -u ${db_A_user} -p${db_A_pass} ${db_A_name} > ${saveBackupPath}/${db_A_name}_backup_${current_time}.sql; then
@@ -13,31 +17,31 @@ if [ -w ${saveBackupPath} ]; then
 
                 if mysql -h ${db_A_host} -u ${db_A_user} -p${db_A_pass} ${db_A_name} < ${saveBackupPath}/${db_B_name}_tmp.sql; then
                      echo "\r\n";
-                     echo "\e[93mSync Suceeded!\e[0m";
+                     echo "\033[93m\033[5mSync Suceeded!\033[25m\033[0m";
                      echo "\r\n";
                 else
                      echo "\r\n";
-                     echo "\e[31mUnable to import Database B into Database A\e[0m";
+                     echo "\033[5m\033[31mUnable to import Database B into Database A\033[0m\033[25m";
                      echo "\r\n";
                 fi
             else
                 echo "\r\n";
-                echo "\e[31mUnable to drop database tables from Database A\e[0m";
+                echo "\033[5m\033[31mUnable to drop database tables from Database A\033[0m\033[25m";
                 echo "\r\n";
             fi
         else
                 echo "\r\n";
-                echo "\e[31mUnable export database Database B"\e[0m;
+                echo "\033[5m\033[31mUnable export database Database B\033[0m\033[25m";
                 echo "\r\n";
         fi
     else
             echo "\r\n";
-            echo "\e[31mUnable to backup database A\e[0m";
+            echo "\033[5m\033[31mUnable to backup database A\033[0m\033[25m";
             echo "\r\n";
     fi
 else
     echo "\r\n"
-    echo "\e[31mYou need to make sure your saveBackupPath in ${dir}/syncdb.cfg is writable.\e[0m"
+    echo "\033[5m\033[31mYou need to make sure your saveBackupPath in ${dir}/syncdb.cfg is writable.\033[0m\033[25m"
     echo "\r\n"
     exit
 fi
