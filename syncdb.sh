4559644b464335db6aab4971857868e3f86da76d
Ubuntu <ubuntu@ip-172-31-44-84.us-west-2.compute.internal>
2017-02-28 22:05:53 +0000

    remove obsolete files

--- syncdb.sh
+++ syncdb.sh
@@ -6,35 +6,45 @@ echo "\r\n";
 echo "\033[93mBeginning Sync Process...\033[0m";
 echo "\r\n";
 
+backupFile=${saveBackupPath}/${db_A_name}_backup_${current_time}.sql
+tmpFile=${saveBackupPath}/${db_B_name}_tmp.sql
+
 if [ -w ${saveBackupPath} ]; then
     ## Backup Current Database
-    if mysqldump -h ${db_A_host} -u ${db_A_user} -p${db_A_pass} ${db_A_name} > ${saveBackupPath}/${db_A_name}_backup_${current_time}.sql; then
+    if mysqldump -h ${db_A_host} -u ${db_A_user} -p${db_A_pass} ${db_A_name} > ${backupFile}; then
     ## Export Database B
-        if mysqldump -h ${db_B_host} -u ${db_B_user} -p${db_B_pass} ${db_B_name} > ${saveBackupPath}/${db_B_name}_tmp.sql; then
+        if mysqldump -h ${db_B_host} -u ${db_B_user} -p${db_B_pass} ${db_B_name} > ${tmpFile}; then
     ## Clear current database.
             if mysql -h ${db_A_host} -u ${db_A_user} -p${db_A_pass} ${db_A_name} < ${dir}/droptables.sql; then
     ## Import Database B into Database A
 
-                if mysql -h ${db_A_host} -u ${db_A_user} -p${db_A_pass} ${db_A_name} < ${saveBackupPath}/${db_B_name}_tmp.sql; then
+                if mysql -h ${db_A_host} -u ${db_A_user} -p${db_A_pass} ${db_A_name} < ${tmpFile}; then
                      echo "\r\n";
                      echo "\033[93m\033[5mSync Suceeded!\033[25m\033[0m";
                      echo "\r\n";
                 else
+                     rm ${backupFile}
+                     rm ${tmpFile}
                      echo "\r\n";
                      echo "\033[5m\033[31mUnable to import Database B into Database A\033[0m\033[25m";
                      echo "\r\n";
                 fi
             else
+                rm ${backupFile}
+                rm ${tmpFile}
                 echo "\r\n";
                 echo "\033[5m\033[31mUnable to drop database tables from Database A\033[0m\033[25m";
                 echo "\r\n";
             fi
         else
-                echo "\r\n";
-                echo "\033[5m\033[31mUnable export database Database B\033[0m\033[25m";
-                echo "\r\n";
+            rm ${backupFile}
+            rm ${tmpFile}
+            echo "\r\n";
+            echo "\033[5m\033[31mUnable export database Database B\033[0m\033[25m";
+            echo "\r\n";
         fi
     else
+            rm ${backupFile}
             echo "\r\n";
             echo "\033[5m\033[31mUnable to backup database A\033[0m\033[25m";
             echo "\r\n";
